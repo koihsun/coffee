6fde0aa60f4c0081d7d965cfb51349cac473579d
koihsun <sag.coi.1215@gmail.com>
2021-05-03 16:22:38 +0800

    Initial commit

---
+++ dist/assets/js/all.js
@@ -0,0 +1,62 @@
+"use strict";
+
+var s;
+
+if ($('#navigation')) {
+  var k = $('#navigation').offset().top;
+  $(window).scroll(function () {
+    s = $(document).scrollTop();
+
+    if (s > k) {
+      $('#navigation').css({
+        "position": "sticky",
+        "top": 0
+      });
+    } else {
+      $('#navigation').css({
+        "position": "static"
+      });
+    }
+  });
+}
+
+if (document.getElementById('sibar')) {
+  var sibar = $('#sibar').offset().top;
+  $(window).scroll(function () {
+    s = $(document).scrollTop();
+
+    if (s > sibar) {
+      $('#sibar__blcok').css({
+        "position": "fixed",
+        "top": "100px"
+      });
+    } else {
+      $('#sibar__blcok').css({
+        "position": "static"
+      });
+    }
+  });
+}
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
+
+if (document.getElementById('level1')) {
+  var level1 = document.getElementById('level1');
+
+  var animate = function animate() {
+    var level11 = document.getElementById('level11');
+    var plus = document.getElementById('plus');
+    level11.className = "sibar__blcok--inner-menu active";
+    plus.className = "plus active";
+  };
+
+  level1.onmouseover = animate;
+}
+//# sourceMappingURL=all.js.map
